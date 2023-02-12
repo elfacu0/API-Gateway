5cd1cb0d599b239fa2fe6da3818f43238f961fc0
“elfacu0” <mahat.mejor@gmail.com>
2023-02-12 09:47:20 -0600

    Add function to get Id to save cache and requests into redis

--- utils/id.go
+++ utils/id.go
@@ -1,5 +1,13 @@
 package utils
 
 func ID(path, method string) string {
-	return path + method
+	return path + "_" + method
+}
+
+func IdCache(path, method string) string {
+	return ID(path, method) + "_CACHE"
+}
+
+func IdRquests(path, method string) string {
+	return ID(path, method) + "_REQUESTS"
 }
