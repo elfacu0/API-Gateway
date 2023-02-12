6629013cdd3bc10e73858ed6b8e48feea4794146
“elfacu0” <mahat.mejor@gmail.com>
2023-02-12 14:48:28 -0600

    Remove unused functions and add function to generate random string

--- utils/id.go
+++ utils/id.go
@@ -1,13 +1,20 @@
 package utils
 
+import (
+	"crypto/rand"
+	"fmt"
+)
+
 func ID(path, method string) string {
 	return path + "_" + method
 }
 
-func IdCache(path, method string) string {
-	return ID(path, method) + "_CACHE"
-}
-
-func IdRquests(path, method string) string {
-	return ID(path, method) + "_REQUESTS"
+func RandomStr() string {
+	b := make([]byte, 10)
+	_, err := rand.Read(b)
+	if err != nil {
+		return ""
+	}
+	str := fmt.Sprintf("%x", b)
+	return str
 }
